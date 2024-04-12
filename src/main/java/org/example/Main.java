5727286414a27b9ed5e7f2180f78780393e01d5e
Dmitrykkjj <lmaoomfg@mail.ru>
2024-04-12 18:16:04 +0500

    brap

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,30 +3,32 @@ package org.example;
 import org.example.dao.BookDao;
 import org.example.dao.BookDaoHibernateImpl;
 import org.example.model.Book;
+import org.example.service.BookService;
+import org.example.service.BookServiceImpl;
 
 
 import java.util.List;
 
 public class Main {
     public static void main(String... arguments) {
-        BookDao bookDao = new BookDaoHibernateImpl();
-        bookDao.dropBookTable();
-        bookDao.createBookTable();
-        bookDao.saveBook("Ernest Miller Hemingway",
+        BookService bookService = new BookServiceImpl();
+        bookService.dropBookTable();
+        bookService.createBookTable();
+        bookService.saveBook("Ernest Miller Hemingway",
                 "The Old Man and the Sea",
                 1952);
-        bookDao.saveBook("Ernest Miller Hemingway",
+        bookService.saveBook("Ernest Miller Hemingway",
                 "The Snows of Kilimanjaro",
                 1936);
-        bookDao.saveBook("Ernest Miller Hemingway",
+        bookService.saveBook("Ernest Miller Hemingway",
                 "Green Hills of Africa",
                 1935);
-        bookDao.removeBook(2);
-        bookDao.saveBook("Ernest Miller Hemingway",
+        bookService.removeBook(2);
+        bookService.saveBook("Ernest Miller Hemingway",
                 "A Moveable Feast",
                 1964);
 
-        List<Book> books = bookDao.getBooks();
+        List<Book> books = bookService.getBooks();
         for (Book book : books) {
             System.out.println(book);
         }
